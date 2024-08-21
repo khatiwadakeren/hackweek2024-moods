47c62c28c79bfd9297ce65acd7620962e24fda3a
Francis Wang <franciswang@NJKNHVVGV9.local>
2024-08-21 16:27:13 -0400

    commit before pull

--- client/src/components/Motivation.jsx
+++ client/src/components/Motivation.jsx
@@ -1,10 +1,22 @@
 import {useState, useEffect} from 'react'
+import emotions from "../../../seeds/motivationalSentences"
 
 const Motivation = () => {
+    const [emotion, setEmotion] = useState("")
+    // Select a random array (angry, happy, neutral)
 
+    useEffect(() => {
+
+        const randomArrayIndex = Math.floor(Math.random() * emotions.length);
+        console.log(randomArrayIndex)
+        const selectedArray = emotions[randomArrayIndex];
+        const randomElementIndex = Math.floor(Math.random() * selectedArray.length);
+        const randomElement = selectedArray[randomElementIndex];
+        setEmotion(randomElement)
+    }, [])
     return (
         <div className='motivation-container'>
-            Motivation Prompt
+            {emotion}
         </div>
     )
 }
