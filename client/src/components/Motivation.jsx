fe630724c0ef0e5f72fc90f27fe12f017f037751
Francis Wang <franciswang@NJKNHVVGV9.local>
2024-08-21 17:55:09 -0400

    finish Motivation component

--- client/src/components/Motivation.jsx
+++ client/src/components/Motivation.jsx
@@ -7,6 +7,7 @@ const Motivation = (emote) => {
 
     useEffect(() => {
         const fetchEmotion = async () => {
+
             try {
                 console.warn(emote.emote)
                 const response = await fetch('http://127.0.0.1:8000/api/emotion-check/', {
@@ -20,7 +21,20 @@ const Motivation = (emote) => {
                 if (response.ok) {
                     const data = await response.json();
                     setEmotion(data.emotion);
-                    console.warn(data)
+                    // console.warn(data)
+                    // console.warn(emotions)
+                    if (data.emotion === "angry") {
+                        const randomIndex = Math.floor(Math.random() * emotions[0].length)
+                        setEmotion(emotions[0][randomIndex])
+                    }
+                    if (data.emotion === "happy") {
+                        const randomIndex = Math.floor(Math.random() * emotions[1].length)
+                        setEmotion(emotions[1][randomIndex])
+                    }
+                    if (data.emotion === "neutral") {
+                        const randomIndex = Math.floor(Math.random() * emotions[2].length)
+                        setEmotion(emotions[2][randomIndex])
+                    }
                 } else {
                     console.error("Failed to fetch emotion:", response.statusText);
                 }
