336f950c6f8b34cc00fb9289cc3d31bc06d533de
Francis Wang <franciswang@NJKNHVVGV9.local>
2024-08-22 14:29:33 -0400

    add sentences

--- seeds/motivationalSentences.js
+++ seeds/motivationalSentences.js
@@ -1,7 +1,7 @@
-const angry = ["You are getting a raise this year!", "At least its not monorepo sourcemaps! (but maybe it is)", "Labor Day weekend is coming up!", "Better go heat up some soothing tea!"]
+const angry = ["You are getting a raise this year!", "At least its not monorepo sourcemaps! (but maybe it is)", "Labor Day weekend is coming up!", "Better go heat up some soothing tea!", "*pats your head*", "it'll be Friday this week!", "Don't Worry! DOUBLE PAY TODAY!!!!"]
 
-const happy = ["Blessed by the Ticket Assignment Gods!", "Almost as good as Pasta Lunch Day!", "In for a treat!"]
+const happy = ["Blessed by the Ticket Assignment Gods!", "Almost as good as Pasta Lunch Day!", "In for a treat!", "Everyone is suffering except you!"]
 
-const neutral = ["This is a neutral ticket", "Feeling sublime", "zzZZZzzzZ"]
+const neutral = ["This is a neutral ticket", "Feeling sublime", "could be worse!"]
 
 export default [angry,happy,neutral]
